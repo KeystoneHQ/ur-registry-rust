903662f540b255fb93a8c09c932b33ab1d098c3f
Zhiying Fan <zhiying@keyst.one>
2022-08-31 01:19:06 +1000

    Add method to support converting compressed key to uncompressed key

--- libs/ur-registry-ffi/src/crypto_hd_key.rs
+++ libs/ur-registry-ffi/src/crypto_hd_key.rs
@@ -1,7 +1,10 @@
-use crate::response::{PtrResponse, Response, Value};
+use crate::response::{PtrResponse, Response};
+use crate::types::PtrString;
 use ur_registry::crypto_hd_key::CryptoHDKey;
 use ur_registry::traits::From;
 use crate::types::PtrVoid;
+use secp256k1::{XOnlyPublicKey, Parity};
+use std::ffi::{CStr};
 
 pub fn resolve(data: Vec<u8>) -> PtrResponse {
     match ur_registry::crypto_hd_key::CryptoHDKey::from_bytes(data) {
@@ -13,6 +16,29 @@ pub fn resolve(data: Vec<u8>) -> PtrResponse {
 #[no_mangle]
 pub extern "C" fn crypto_hd_key_get_key_data(crypto_hdkey: &mut CryptoHDKey) -> PtrResponse {
     Response::success_string(hex::encode(crypto_hdkey.get_key())).c_ptr()
+}
+
+#[no_mangle]
+pub extern "C" fn crypto_hd_key_get_uncompressed_key_data(compressed_key: PtrString) -> PtrResponse {
+    let key_cstr = unsafe { CStr::from_ptr(compressed_key) };
+    let key_string = match key_cstr.to_str() {
+        Ok(value) => value.to_string(),
+        Err(error) => return Response::error(error.to_string()).c_ptr(),
+    };
+    
+    let slice = &key_string.clone()[2..];    
+    let decoded_slice = hex::decode(slice).unwrap();
+    
+    let result = match XOnlyPublicKey::from_slice(&decoded_slice) {
+        Ok(res) => res,
+        Err(error) => return Response::error(error.to_string()).c_ptr(),
+    };
+    
+    let prefix = &key_string[..2];
+    let parity = if prefix.eq("02") { Parity::Even } else { Parity::Odd };
+
+    let uncompressed_key = result.public_key(parity).serialize_uncompressed();
+    Response::success_string(hex::encode(uncompressed_key)).c_ptr()
 }
 
 #[no_mangle]
