6088fe34bdf2d250b57b9510bbc5a72fc901315f
Charon-Fan <Charon.Dian@gmail.com>
2024-08-21 21:38:49 +0800

    chore: Update qr_code_scanner_plus dependency to the latest version

--- interfaces/ur_registry_flutter/lib/components/animated_qr_scanner.dart
+++ interfaces/ur_registry_flutter/lib/components/animated_qr_scanner.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
 import 'package:ur_registry_flutter/native_object.dart';
 import 'package:ur_registry_flutter/ur_decoder.dart';
 
